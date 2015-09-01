7f6f256cabf0f17fc46d3cb901a6c14194c0fd0f
pk400 <joelaro@outlook.com>
2015-08-31 22:56:30 -0400

    not sure what to do with camera still, keeping a temporary view in playstate for now

--- include/PlayState.hpp
+++ include/PlayState.hpp
@@ -13,6 +13,7 @@ private:
     vector<sf::Sprite> platSprites_;
     sf::Text* debug_input;
 public:
+    sf::View view; // Temp view
     PlayState();
     int processInput(sf::Event event, sf::Vector2i mouse);
     void update(float dt);
