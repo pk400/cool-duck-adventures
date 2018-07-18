fb52b562f394a098b24161cb14f5e02bacbb4205
Joel Aro <jaro@Astraea.local>
2018-07-17 22:26:59 -0400

    Added toml and created a Settings object in Main.cpp.
    Encapsulating settings within a Settings struct makes it much easier to manage and pass around to other functions. I am using TOML for the configuration file.

--- src/Game.hpp
+++ src/Game.hpp
@@ -2,18 +2,18 @@
 #define GAME_HPP
 
 #include "GSM.hpp"
+#include "Settings.hpp"
 
 class Game {
 public:
-    Game(int game_width, int game_height, std::string game_title);
+    Game(Settings settings);
     int gameRun();
     void handleEvents();
     void update(float dt);
     void render();
     sf::RenderWindow& getContextWindow()    { return *window; }
 private:
-    const int gamewidth;
-    const int gameheight;
+    Settings gs;
     sf::RenderWindow* window;
     GSM* gsm;
     float dt;
